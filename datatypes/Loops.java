3c7dd42e670e51e3dc4077fee7f2202c56e32648
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-14 13:53:59 +0530

    Loops.java

--- datatypes/Loops.java
+++ datatypes/Loops.java
@@ -5,11 +5,13 @@ public class Loops{
     //print "Hello World" 5 times
     int counter = 0;
     while (counter<5){
-      System.out.println('Hello World');
+      System.out.println("Hello World");
       counter++;
     }
+    
     //print number from 1 to n
     Scanner sc= new Scanner(System.in);
+    System.out.println("Enter the value of n ")
     int range= sc.nextInt();
     int c=1;
     while (c<= range){
@@ -21,6 +23,23 @@ public class Loops{
     for(int i=1; i<=5; i++){
       System.out.println("Hello World");
     }
-    
+
+    //sum of first n natural numbers
+    System.out.println("Enter the value of n ");
+    int n=sc.nextInt();
+    int sum=0;
+    int i=1;
+    while(i<=n){
+      sum=sum+i;
+      i++;
+    }
+    System.out.println("sum of first n natural numbers is " + sum);
+  
+  //DO-WHILE LOOP
+    int d=1;
+    do{
+      System.out.println("Hello");
+      d++;
+    } while (d<=5);
   }
 }
