e2a75a2e2901e0366e0afb328a034f00ddd0750a
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-16 11:13:07 +0530

    Inverted Half Pyramid Pattern

---
+++ Patterns/InvertedPyramid.java
@@ -0,0 +1,19 @@
+public class InvertedPyramid{
+  public static void inverted_half_pyramid(int n){
+    //outer
+    for (int i=1; i<=n; i++){
+      //spaces
+      for (int j=1; j<=n-i; j++){
+        System.out.print(" ");
+      }
+      //stars
+      for (int j=1; j<=i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String args[]){
+    inverted_half_pyramid(5);
+  }
+}
