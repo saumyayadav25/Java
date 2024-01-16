edad7c880f3afbaeb8c89367eb1855175a605c0c
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-16 10:22:01 +0530

    Inverted Star Pattern

---
+++ Patterns/InvertedStar.java
@@ -0,0 +1,10 @@
+public class InvertedStar{
+  public static void main(String args[]){
+    for(int i=1; i<=4; i++){
+      for(int s=1; s<=(4-i+1); s++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
