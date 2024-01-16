052d6ee8fcc6c02279dfa8a8bd378eef20c52d8d
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-16 10:15:50 +0530

    print Star Pattern

---
+++ Patterns/Star.java
@@ -0,0 +1,11 @@
+java.util.*;
+public class Star{
+  public static void main(String args[]){
+    for (int i=1;i<=4; i++){
+      for (int star=1; star<=i; star++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
