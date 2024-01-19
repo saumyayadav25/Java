f46187f10d9e0e2caec03a86bc13e299089da62e
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-19 12:05:04 +0530

    create Function to swap two numbers

---
+++ Functions/Swap.java
@@ -0,0 +1,12 @@
+public class Swap{
+  public static void swap(int a, int b){
+    int temp=a;
+    a=b;
+    b=temp;
+    System.out.println("a=" + a);
+    System.out.println("b=" + b);
+  }
+  public static void main(String args[]){
+    swap(5,10);
+  }
+}
