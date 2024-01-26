dbb85f08b2f56381c81391ae582774f9352f1378
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-26 20:37:37 +0530

    Create Args.java
    Passing arrays as argument

---
+++ Args.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Args{
+  public static void updat(int marks[]){
+    for(int i=0; i<marks.length;i++){
+      marks[i]=marks[i]+1;
+    }
+  }
+  public static void main(String args[]){
+    int marks[]={95,98,99};
+    updat(marks);
+    for(int i=0; i<marks.length;i++){
+      Syste.out.print(marks[i]+" ")
+    }
+    System.out.println();
+  }
+}
