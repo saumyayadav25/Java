f0de91276a56971a4265a3933aa979766d67de3f
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-27 19:05:55 +0530

    Create BinaryS.java
    Binary Search

---
+++ Arrays/BinaryS.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class BinaryS{
+  public static int binarySearch(int numbers[], int key){
+    int start=0, end= numbers.length-1;
+    while (start<=end){
+      int mid=(start+end)/2;
+      //comparisons
+      if (numbers[mid]==key){
+        return mid;
+      }
+      if (numbers[mid]<key){ //right
+        start=mid+1;
+      } else{ //left
+        end=mid-1;
+      }
+    }
+    return -1; //if key doesnt exist in array
+  }
+  public static void main(String args[]){
+    int numbers[]={2,4,6,8,10,12,14};
+    int key=10;
+    System.out.println("Index for key is " + binarySearch(numbers,key));
+  } 
+}
