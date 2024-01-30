8028d5b25de050dd6832249fb93b27141550756b
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-30 19:53:42 +0530

    create BasicSort.java

--- Sorting algorithms/Selection sort/BasicSort.java
+++ Sorting algorithms/Selection sort/BasicSort.java
@@ -3,8 +3,8 @@ public class BasicSort{
   public static void selectionSort(int arr[]){
     for (int i=0; i<arr.length-1;i++){
       int minPos=i;
-      for(int j=i+1; j<arr.length-1;j++){
-        if (arr[minPos]> arr[j]){
+      for(int j=i+1; j<arr.length;j++){
+        if (arr[minPos]> arr[j]){ //arr[minPos]<arr[j] for descending order
           minPos=j;
         }
       }
@@ -13,6 +13,12 @@ public class BasicSort{
       arr[minPos]=arr[i];
       arr[i]=temp;
     }
+  }
+  public static void printArr(int arr[]){
+    for (int i=0; i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+    System.out.println();
   }
   public static void main(String args[]){
     int arr[]={5,4,1,3,2};
