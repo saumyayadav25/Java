bc59028e71b580a724c27c83c505e94a0d843556
saumyayadav25 <141031443+saumyayadav25@users.noreply.github.com>
2024-01-30 20:14:36 +0530

    Create InbuiltSort.java

---
+++ Sorting algorithms/InbuiltSort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class InbuiltSort{
+  public static void printArr(int arr[]){
+    for (int i=0; i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+  }
+  public static void main(String args[]){
+    int arr[]={5,4,1,3,2};
+    Arrays.sort(arr);
+    //Arrays.sort(arr,0,3); //gives output 1 4 5 3 2
+    printArr(arr);
+  }
+}
